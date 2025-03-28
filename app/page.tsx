76ca6ff22817bb0458fb1471cf77b50757dea370
Sochi <104289670+MallardxGreen@users.noreply.github.com>
2025-03-28 15:53:32 -0700

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,3 @@
-"use client";
-
-import { useState, useEffect } from "react";
-import { generateClient } from "aws-amplify/data";
-import type { Schema } from "@/amplify/data/resource";
-import "./../app/app.css";
-import { Amplify } from "aws-amplify";
-import outputs from "@/amplify_outputs.json";
-import "@aws-amplify/ui-react/styles.css";
-
-Amplify.configure(outputs);
-
-const client = generateClient<Schema>();
-
 export default function App() {
   const [todos, setTodos] = useState<Array<Schema["Todo"]["type"]>>([]);
 
@@ -31,13 +17,24 @@ export default function App() {
     });
   }
 
+  // Add this new function
+  function deleteTodo(id: string) {
+    client.models.Todo.delete({ id });
+  }
+
   return (
     <main>
       <h1>My todos</h1>
       <button onClick={createTodo}>+ new</button>
       <ul>
         {todos.map((todo) => (
-          <li key={todo.id}>{todo.content}</li>
+          <li
+            onClick={() => deleteTodo(todo.id)}
+            key={todo.id}
+            style={{ cursor: 'pointer' }}
+          >
+            {todo.content}
+          </li>
         ))}
       </ul>
       <div>
